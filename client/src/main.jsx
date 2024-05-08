a78a47c5d5d541e904d989e55f085c7ce6c80760
MarGoN <monondcosta@gmail.com>
2024-05-08 20:29:40 +0600

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1 className='font-lato text-3xl'>running client of SkillNexus</h1>
+  </React.StrictMode>,
+)
